9ba97514882355f25adc9c17baee77e5c22e4496
ChunChenLin <tp632333846@gmail.com>
2016-03-14 22:33:08 +0800

    add memory.h & memory.cpp which are for dealing with loading/storing.

---
+++ 103062302_01/simulator/memory.cpp
@@ -0,0 +1,3 @@
+#include "memory.h"
+
+map<int, vector<int>> Memory::memory;
---
+++ 103062302_01/simulator/memory.h
@@ -0,0 +1,12 @@
+#ifndef memory_h
+#define memory_h
+
+#include<map>
+#include<vector>
+
+using namespace std;
+
+class Memory {
+public:
+    map<int, vector<int>> memory;
+}
