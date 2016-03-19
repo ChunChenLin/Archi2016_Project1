c223deb0d28c956c306284c153b9a482d1914605
ChunChenLin <tp632333846@gmail.com>
2016-03-20 00:24:48 +0800

    add reslove.h & resolve.cpp for tracing the instrucion.

---
+++ 103062302_01/simulator/resolve.cpp
@@ -0,0 +1,17 @@
+#include "resolve.h"
+
+void RsRtRd(unsigned *rs, unsigned *rt, unsigned *rd) {
+
+}
+
+void Shamt(unsigned *shamt) {
+
+}
+
+void UnsignedImmediate(unsigned *immediate) {
+
+}
+
+void SignedImmediate(unsigned *immediate) {
+	
+}
---
+++ 103062302_01/simulator/resolve.h
@@ -0,0 +1,9 @@
+#include "global.h"
+
+void RsRtRd(unsigned *rs, unsigned *rt, unsigned *rd);
+
+void Shamt(unsigned *shamt);
+
+void UnsignedImmediate(unsigned *immediate);
+
+void SignedImmediate(unsigned *immediate);
