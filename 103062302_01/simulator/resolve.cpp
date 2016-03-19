f4af3d453617cc86aa34e8f5531f0bc9fc3de3a4
ChunChenLin <tp632333846@gmail.com>
2016-03-20 01:53:37 +0800

    resolve.cpp done!

--- 103062302_01/simulator/resolve.cpp
+++ 103062302_01/simulator/resolve.cpp
@@ -1,17 +1,56 @@
 #include "resolve.h"
 
-void RsRtRd(unsigned *rs, unsigned *rt, unsigned *rd) {
+void Opcode(unsigned *op) {
+	op = IMemory[PC];
+	op = op >> 2 << 26 >> 26;
+}
 
+void RsRtRd(unsigned *rs, unsigned *rt, unsigned *rd) {
+	unsigned t1 = IMemory[PC], t2 = IMemory[PC+1];
+	t1 = t1 << 30 >> 27;
+	t2 = t2 << 24 >> 29;
+	*rs = t1 + t2;
+
+	*rt = IMemory[PC+1];
+	*rt = (*rt) << 27 >> 27;
+
+	if(rd!=NULL) {
+		*rd = IMemory[PC+2];
+		*rd = (*rd) << 24 >> 27;
+	}
 }
 
 void Shamt(unsigned *shamt) {
+	unsigned t1 = IMemory[PC+2], t2 = IMemory[PC+3];
+	t1 = t1 << 29 >> 27;
+	t2 = t2 >> 6 << 30 >> 30;
+	*shamt = t1 + t2;
+}
 
+void Func(unsigned *func) {
+	func = IMemory[PC+3];
+	func = func << 26 >> 26;
 }
 
 void UnsignedImmediate(unsigned *immediate) {
-
+	unsigned t1 = IMemory[PC+2], t2 = IMemory[PC+3];
+	t1 = t1 << 24 >> 16;
+	t2 = t2 << 24 >> 24;
+	*immediate = t1 + t2;
 }
 
 void SignedImmediate(unsigned *immediate) {
-	
+	unsigned t1 = IMemory[PC+2], t2 = IMemory[PC+3];
+	t1 = t1 << 24 >> 16;
+	t2 = t2 << 24 >> 24;
+	*immediate = (t1 + t2) << 16 >> 16;
+}
+
+void Address(unsigned *address) {
+	unsigned t1 = IMemory[PC], t2 = IMemory[PC+1], t3 = IMemory[PC+2], t4 = IMemory[PC+3];
+	t1 = t1 << 30 >> 6;
+	t2 = t2 << 24 >> 8;
+	t3 = t3 << 24 >> 16;
+	t4 = t4 << 24 >> 24;
+	*address = t1 + t2 + t3 + t4;
 }
--- 103062302_01/simulator/resolve.h
+++ 103062302_01/simulator/resolve.h
@@ -1,9 +1,15 @@
 #include "global.h"
 
+void Opcode(unsigned *op);
+
 void RsRtRd(unsigned *rs, unsigned *rt, unsigned *rd);
 
 void Shamt(unsigned *shamt);
 
+void Func(unsigned *func);
+
 void UnsignedImmediate(unsigned *immediate);
 
 void SignedImmediate(unsigned *immediate);
+
+void Address(unsigned *address);
